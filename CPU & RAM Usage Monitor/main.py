a5e665d076c588f9df24e298570c5b6584aa5898
Nabil Hakimi <muhamadnabilhakimi@gmail.com>
2022-11-21 22:34:31 +0800

    Add CPU&MEM Usage Monitor project

---
+++ CPU & RAM Usage Monitor/main.py
@@ -0,0 +1,16 @@
+import time
+import psutil
+
+def display_usage(cpu_usage, mem_usage, bars=50):
+    cpu_percent = (cpu_usage / 100.0)
+    cpu_bar = '#' * int(cpu_percent * bars) + '-' * (bars - int(cpu_percent * bars))
+    
+    mem_percent = (mem_usage / 100.0)
+    mem_bar = '#' * int(mem_percent * bars) + '-' * (bars - int(mem_percent * bars))
+    
+    print(f"\rCPU USAGE: |{cpu_bar}| {cpu_usage:.2f}%  ", end="")
+    print(f"MEM USAGE: |{mem_bar}| {mem_usage:.2f}%  ", end="\r")
+    
+while True:
+    display_usage(psutil.cpu_percent(), psutil.virtual_memory().percent, 30)
+    time.sleep(1)
