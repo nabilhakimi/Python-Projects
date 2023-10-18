43585cadd68ca1556e3be7f2a78b367e9250a3d9
Nabil Hakimi <muhamadnabilhakimi@gmail.com>
2023-10-18 23:06:06 +0800

    Added valid images format

--- Image Processor/main.py
+++ Image Processor/main.py
@@ -3,9 +3,15 @@ import os
 
 source_directory = './Images_Sources'
 edited_directory = '/Edited_Images'
+valid_image = []
+valid_image_format = [".jpg", ".gif", ".png", ".tga"]
 
 for images in os.listdir(source_directory):
+    ext = os.path.splitext(images)[1]
+    if ext.lower() not in valid_image_format:
+        continue
     image = Image.open(f"{source_directory}/{images}")
+    valid_image.append(image)
 
     edit = image.filter(ImageFilter.SHARPEN)
     clean_name = os.path.splitext(images)[0]
